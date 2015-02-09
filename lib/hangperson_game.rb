3005725e2441d32919a1e9f792e6c45c71332150
Riyaz Shah <riyazshah@berkeley.edu>
2015-02-09 03:35:04 -0400

    completed features

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -4,12 +4,67 @@ class HangpersonGame
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  def initialize(new_word)
+    @word = new_word 
+    @guesses = ""
+    @wrong_guesses = ""
+  end
 
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
+  end
+
+  def guess(letter)
+    if letter == nil
+      raise ArgumentError.new('letter is nil')
+    elsif letter == ''
+      raise ArgumentError.new('cannot enter empty letter')
+    elsif !(letter.ord.between?(65,90) or letter.ord.between?(97,122))
+      raise ArgumentError.new('letter must be a character')
+    end
+    if @word.include? letter
+      if !(@guesses.include? letter)
+        @guesses += letter
+      else
+        return false
+      end
+    else
+      if !(@wrong_guesses.include? letter)
+        @wrong_guesses += letter
+      else
+        return false
+      end
+    end
+  end
+
+  def word_with_guesses
+    temp = ""
+    @word.each_char do |i|
+      if @guesses.include? i
+        temp += i
+      else
+        temp += '-'
+      end
+    end
+    return temp
+  end
+
+  def check_win_or_lose
+    if @wrong_guesses.length == 7
+      return :lose
+    end
+    @word.each_char do |i|
+      if !(@guesses.include? i)
+        return :play
+      end
+    end
+    return :win
   end
 
 end
